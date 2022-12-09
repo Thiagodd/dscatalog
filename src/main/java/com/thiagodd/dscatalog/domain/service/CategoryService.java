1ed711f17f2af0f9566aadf4044fedd50fdfe3e2
Thiago Silva <119633375+Thiagodd@users.noreply.github.com>
2022-12-09 08:00:19 -0300

    Database seeding

--- src/main/java/com/thiagodd/dscatalog/domain/service/CategoryService.java
+++ src/main/java/com/thiagodd/dscatalog/domain/service/CategoryService.java
@@ -2,21 +2,33 @@ package com.thiagodd.dscatalog.domain.service;
 
 import com.thiagodd.dscatalog.domain.model.Category;
 import com.thiagodd.dscatalog.domain.repository.CategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
-
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> category = categoryRepository.findById(id);
+        if(category.isPresent()){
+            return category.get();
+        }
+        throw new EntityNotFoundException("Entidade não encontrada");
     }
 
 
