1fbc1831de418fccd9ce69b0a901d4ad22fb0df9
Thiago Silva <119633375+Thiagodd@users.noreply.github.com>
2022-12-09 07:02:51 -0300

    FindById

--- src/main/java/com/thiagodd/dscatalog/api/controller/CategoryController.java
+++ src/main/java/com/thiagodd/dscatalog/api/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.thiagodd.dscatalog.api.controller;
 import com.thiagodd.dscatalog.domain.model.Category;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class CategoryController {
         categoryList.add(new Category(1L, "Books"));
         categoryList.add(new Category(2L, "Electronics"));
         return ResponseEntity.ok().body(categoryList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(new Category(1L, "Books"));
     }
 }
